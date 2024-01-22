4cf41d0616d4a0df5c50781180588bcf5324cb87
Felix Hahn <info@hahn-felix.de>
2024-01-22 23:16:53 +0100

    Update add-get-params-to-search.php

--- includes/add-get-params-to-search.php
+++ includes/add-get-params-to-search.php
@@ -1,8 +1,16 @@
 <?php
-
 /**
- * Add GET parameters to SQL condition if safe
- * currently supported: category by tag_id, scope, country by 2 letter uppercase code
+ * Add GET parameters to search arguments if set and safe.
+ * - category by tag_id
+ * - scope
+ * - country by 2 letter uppercase code
+ * 
+ * Note 1: The search form always recognizes the category by tag_id and the country by 2 letter uppercase code. That means it shows those as pre-selected in the advanced search however the events are not filtered accordingly.
+ * We just make this work completely.
+ * 
+ * Note 2: The scope won't be visible as selected in the search form.
+ * 
+ * @todo Remove URL parameters when they have been changed, via JS.
 */
 add_filter( 'em_events_get_default_search', 'add_get_params', 10, 3);
 function add_get_params( $args, $array, $defaults ):array
@@ -15,7 +23,7 @@ function add_get_params( $args, $array, $defaults ):array
 	if (isset( $_GET['scope']) && in_array($_GET['scope'], array_keys(em_get_scopes()))) {
 		$args['scope'] = $GET['scope'];
 	}
-	// Country by to letter code (uppercase !)
+	// Country by to letter uppercase code
 	if (isset($_GET['country']) && (strlen($_GET['country']) == 2) && ctype_upper($_GET['country'])) {
 		$args['country'] = $_GET['country'];
 	}
