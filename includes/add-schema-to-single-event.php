1ee68cc3ed4a07ec0125a52b1b69ab985c133322
Felix Hahn <info@hahn-felix.de>
2024-01-30 20:44:29 +0100

    Update add-schema-to-single-event.php

--- includes/add-schema-to-single-event.php
+++ includes/add-schema-to-single-event.php
@@ -1,42 +1,105 @@
 <?php
 
+/*
+ * Add JSON-LD formatted Schema.org data to the head section of single event pages
+ */
 add_action( 'wp_head', 'fx_em_schema_single' );
 function fx_em_schema_single() {
 
     if ( get_post_type() === 'event' ) {
-        
-		$EM_Event = em_get_event(get_the_ID());
-        
-        $schema['@context'] = 'https::schema.org';
-        $schema['@type'] = 'Event';
-		
-		$schema['name'] = $EM_Event->name;
-        
-        // @todo support online events
-        $schema['location'] = get_location( $EM_Event->location_id );
-        
-        $schema = json_encode($schema, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-        
-        echo "<script type='application/ld+json'>{$schema}</script>\n";
+
+        $FX_EM_Schema = new FX_EM_Schema( get_the_id() );
+        echo "<script type='application/ld+json'>{$FX_EM_Schema->get_json_string()}</script>\n";
     }
 }
 
-function get_location( $location_id ): array {
-
-    $EM_Location = em_get_location( $location_id );
-	//$location = (array) $EM_Location;
+class FX_EM_Schema {
     
-    $location['@type'] = 'Place';
-    $location['name'] = $EM_Location->location_name;
-        
-    $location['address'] = get_address($EM_Location);
-        
-    return $location;
-}
-
-function get_address( $EM_Location ): array {
-
-    $address['@type'] = 'PostalAddress';
+    /**
+     * @var int
+     */
+    public int $post_id;
     
-    return $address;
+    /**
+	 * @var EM_Event
+	 */
+    public $EM_Event;
+    
+	/**
+	 * @var EM_Location
+	 */
+    public $EM_Location;
+    
+    public function __construct( int $post_id ) {
+        
+        $this->post_id = $post_id;
+        $this->EM_Event = em_get_event( $this->post_id, 'post_id' );
+        $this->EM_Location = em_get_location( $this->EM_Event->location_id );
+    }
+    
+    /**
+     * Get the Schema in JSON-LD format
+     */
+    public function get_json_string(): string {
+        
+        $schema = $this->getSchema();
+        $schema = json_encode($schema, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        
+        return $schema;
+    }
+    
+    /*
+     * Get the Schema as array
+     */
+    private function getSchema(): array {
+        
+        $schema['@context'] = 'https://schema.org/';
+        $schema['@type'] = 'Event';
+        
+        // Event Name
+        $schema['name'] = $this->EM_Event->name;
+
+        // Start Date and Time
+        $EM_DateTime = $this->EM_Event->start();
+		$schema['startDate'] = $EM_DateTime->format('Y-m-d\TH:iO');
+        
+        // End Date and Time
+        $EM_DateTime = $this->EM_Event->end();
+        $schema['endDate'] = $EM_DateTime->format('Y-m-d\TH:iO');
+
+        // Location
+        // @todo support online events
+        $schema['location'] = $this->get_location();
+        
+        return $schema;
+    }
+    
+    /*
+     * Get Location Schema as array
+     */
+    private function get_location(): array {
+        
+        $location['@type'] = 'Place';
+        $location['name'] = $this->EM_Location->location_name;
+
+        // Address
+        $location['address'] = (array) $this->get_address();
+            
+        return $location;
+    }
+
+    /*
+     * Get Address Schema as array
+     */
+    private function get_address(): array {
+
+        $address['@type'] = 'PostalAddress';
+        $address['addressCountry'] = $this->EM_Location->location_country;
+        $address['addressLocality'] = $this->EM_Location->location_town;
+        $address['addressRegion'] = $this->EM_Location->location_region;
+        $address['postalCode'] = $this->EM_Location->location_postcode;
+        $address['streetAddress'] = $this->EM_Location->location_address;
+        
+        return $address;
+    }
 }
